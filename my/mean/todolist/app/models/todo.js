a710190ae146b5cc95030a02b9e7f56f98cee756
Sumit Kohli <sumit@xicom.biz>
2015-05-05 19:05:00 +0530

    latest

--- my/mean/todolist/app/models/todo.js
+++ my/mean/todolist/app/models/todo.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema ({
-    'text' : String,
+    text : {
+        type : String,
+        //required : 'Text is required'
+    },
     'age' : String
 });
 
